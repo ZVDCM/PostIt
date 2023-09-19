64ceafff82dfa149c0ba3942d1a620f1c6b8900c
Zehrei Verj <moraleszehreiverj8@gmail.com>
2023-09-19 20:37:41 +0800

    finish not found ui design. add background in account ui

--- PostIt.Frontend/src/app/features/not-found/not-found.module.ts
+++ PostIt.Frontend/src/app/features/not-found/not-found.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotfoundComponent } from './not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [NotfoundComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     exports: [NotfoundComponent],
 })
 export class NotfoundModule {}
