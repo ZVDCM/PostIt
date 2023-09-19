111f9842ef7dab69675f6f6d96bf8ee024b1bc5f
Zehrei Verj <moraleszehreiverj8@gmail.com>
2023-09-19 20:37:01 +0800

    finish not found ui design. add background in account ui

--- PostIt.Frontend/src/app/features/account/account.component.ts
+++ PostIt.Frontend/src/app/features/account/account.component.ts
@@ -3,22 +3,39 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     selector: 'app-account',
     template: `
+        <span
+            class="fixed top-[-8rem] left-0 text-[20rem] font-extrabold tracking-widest whitespace-nowrap text-slate-900 select-none"
+        >
+            POST IT
+        </span>
+        <span
+            class="fixed bottom-[-5rem] right-0 text-[20rem] font-extrabold tracking-widest whitespace-nowrap text-slate-900 select-none"
+        >
+            POST IT
+        </span>
         <router-outlet />
         <footer
-            class="flex flex-col items-center mt-auto h-[15%] font-extrabold tracking-[.3em]"
+            class="flex flex-col items-center mt-auto h-[15%] font-extrabold text-slate-600 z-[1]"
         >
-            <span>ZVDCM</span>
-            <span>2023</span>
+            <div class="space-x-2">
+                <span>Z</span>
+                <span>V</span>
+                <span>D</span>
+                <span>C</span>
+                <span>M</span>
+            </div>
+            <div class="space-x-2">
+                <span>2</span>
+                <span>0</span>
+                <span>2</span>
+                <span>3</span>
+            </div>
         </footer>
     `,
     styles: [
         `
             :host {
                 @apply h-full flex flex-col justify-center items-center;
-
-                footer > span {
-                    color: var(--surface-border);
-                }
             }
         `,
     ],
--- PostIt.Frontend/src/app/features/not-found/not-found.component.ts
+++ PostIt.Frontend/src/app/features/not-found/not-found.component.ts
@@ -1,9 +1,44 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountConstantsService } from 'src/app/shared/constants/account-constants.service';
 
 @Component({
     selector: 'app-notfound',
-    template: ` <p>notfound</p> `,
-    styles: [],
+    template: `
+        <div
+            class="flex flex-col justify-center items-center gap-4 font-extrabold"
+        >
+            <h1 class="text-7xl tracking-widest">ROUTE NOT FOUND</h1>
+            <h3
+                (click)="router.navigate([accountConstants.loginEndpoint])"
+                class="text-5xl tracking-widest text-slate-800 cursor-pointer"
+            >
+                POST <span> IT </span>
+            </h3>
+            <span class="text-xl animate-bounce">^</span>
+        </div>
+    `,
+    styles: [
+        `
+            :host {
+                @apply h-full flex justify-center items-center;
+
+                h3:hover {
+                    color: white;
+
+                    span {
+                        color: var(--primary-color);
+                    }
+                }
+            }
+        `,
+    ],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [AccountConstantsService],
 })
-export class NotfoundComponent {}
+export class NotfoundComponent {
+    constructor(
+        public accountConstants: AccountConstantsService,
+        public router: Router
+    ) {}
+}
