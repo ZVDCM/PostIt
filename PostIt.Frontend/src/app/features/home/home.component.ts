f06c0c2370320bb27d177e55c08d37b4aa66b080
Zehrei Verj <moraleszehreiverj8@gmail.com>
2023-09-21 06:46:49 +0800

    add user async pipe

--- PostIt.Frontend/src/app/features/home/home.component.ts
+++ PostIt.Frontend/src/app/features/home/home.component.ts
@@ -1,93 +1,116 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { MenuItem } from 'primeng/api';
+import { Observable } from 'rxjs';
 import { selectAccessToken } from 'src/app/core/state/access-token/access-token.selectors';
+import { IUserState } from 'src/app/core/state/user/user.model';
 import { selectUser } from 'src/app/core/state/user/user.selectors';
+import { AccountConstantsService } from 'src/app/shared/constants/account-constants.service';
 import { HomeConstantsService } from 'src/app/shared/constants/home-constants.service';
 
 @Component({
     selector: 'app-home',
     template: `
-        <aside
-            class="h-screen sticky top-0 flex flex-col flex-1 items-end"
-            style="border-right: 1px solid var(--surface-border); background-color: var(--surface-ground)"
-        >
-            <nav class="h-full flex flex-col gap-5 py-10 pl-10">
-                <header
-                    [routerLink]="homeConstants.homeEndpoint"
-                    class="cursor-pointer mb-14 pr-10 focus:outline-none"
-                >
-                    <h1
-                        class="text-6xl font-extrabold tracking-widest rounded-none"
-                    >
-                        POST <span style="color: var(--primary-color)">IT</span>
-                    </h1>
-                </header>
-                <button
-                    pButton
-                    class="nav-button"
-                    [ngClass]="{ active: isPostsActive }"
-                    (click)="onClickPosts()"
-                >
-                    <i class="pi pi-camera"></i>
-                    <span>Posts</span>
-                </button>
-                <button
-                    pButton
-                    class="nav-button"
-                    [ngClass]="{ active: isProfileActive }"
-                    (click)="onClickProfile()"
-                >
-                    <i class="pi pi-user"></i>
-                    <span>Profile</span>
-                </button>
-                <div class="mt-auto mr-11">
-                    <p-splitButton
-                        class="w-full"
-                        styleClass="w-full"
-                        [menuStyle]="{ width: '100%' }"
-                        label="JuanDelaCruz"
-                        icon="pi pi-at"
-                        [model]="items"
-                    >
-                        <ng-template pTemplate="dropdownicon">
-                            <i class="pi pi-chevron-up"></i>
-                        </ng-template>
-                    </p-splitButton>
-                </div>
-            </nav>
-        </aside>
-        <div
-            class="h-full w-full max-w-3xl"
-            style="border-right: 1px solid var(--surface-border); background-color: var(--surface-ground)"
-        >
-            <router-outlet />
-        </div>
-        <aside class="h-screen sticky top-0 flex-1">
-            <div class="flex flex-col p-10">
-                <div class="p-inputgroup">
-                    <input id="txt-search" pInputText placeholder="Search" />
-                    <button type="button" pButton icon="pi pi-search"></button>
-                </div>
-            </div>
-            <div
-                class="absolute bottom-0 right-0 flex flex-col items-end p-12 font-extrabold text-slate-600"
+        <ng-container *ngIf="user$ | async as user">
+            <aside
+                class="h-screen sticky top-0 flex flex-col flex-1 items-end"
+                style="border-right: 1px solid var(--surface-border); background-color: var(--surface-ground)"
             >
-                <div class="space-x-2">
-                    <span>Z</span>
-                    <span>V</span>
-                    <span>D</span>
-                    <span>C</span>
-                    <span>M</span>
-                </div>
-                <div class="space-x-2">
-                    <span>2</span>
-                    <span>0</span>
-                    <span>2</span>
-                    <span>3</span>
-                </div>
+                <nav class="h-full flex flex-col gap-5 py-10 pl-10">
+                    <header
+                        [routerLink]="homeConstants.homeEndpoint"
+                        class="cursor-pointer mb-14 pr-10 focus:outline-none"
+                    >
+                        <h1
+                            class="text-6xl font-extrabold tracking-widest rounded-none"
+                        >
+                            POST
+                            <span style="color: var(--primary-color)">IT</span>
+                        </h1>
+                    </header>
+                    <button
+                        [ngClass]="{ active: isPostsActive }"
+                        (click)="onClickPosts()"
+                        pButton
+                        class="nav-button"
+                        icon="pi pi-camera"
+                    >
+                        <span>Posts</span>
+                    </button>
+                    <button
+                        [ngClass]="{ active: isProfileActive }"
+                        (click)="onClickProfile()"
+                        pButton
+                        class="nav-button"
+                        icon="pi pi-user"
+                    >
+                        <span>Profile</span>
+                    </button>
+                    <div class="mt-auto mr-11">
+                        <p-splitButton
+                            class="w-full"
+                            styleClass="w-full"
+                            [menuStyle]="{ width: '100%' }"
+                            label="JuanDelaCruz"
+                            [model]="items"
+                        >
+                            <ng-template
+                                pTemplate="content"
+                            >
+                                <div class="flex items-center gap-4">
+                                    <i class="pi pi-at p-button-icon"></i>
+                                    <span class="p-button-label">{{
+                                        user?.value?.username
+                                    }}</span>
+                                </div>
+                            </ng-template>
+                            <ng-template pTemplate="dropdownicon">
+                                <i class="pi pi-chevron-up"></i>
+                            </ng-template>
+                        </p-splitButton>
+                    </div>
+                </nav>
+            </aside>
+            <div
+                class="h-full w-full max-w-3xl"
+                style="border-right: 1px solid var(--surface-border); background-color: var(--surface-ground)"
+            >
+                <router-outlet />
             </div>
-        </aside>
+            <aside class="h-screen sticky top-0 flex-1">
+                <div class="flex flex-col p-10">
+                    <div class="p-inputgroup">
+                        <input
+                            id="txt-search"
+                            pInputText
+                            placeholder="Search"
+                        />
+                        <button
+                            type="button"
+                            pButton
+                            icon="pi pi-search"
+                        ></button>
+                    </div>
+                </div>
+                <div
+                    class="absolute bottom-0 right-0 flex flex-col items-end p-12 font-extrabold text-slate-600"
+                >
+                    <div class="space-x-2">
+                        <span>Z</span>
+                        <span>V</span>
+                        <span>D</span>
+                        <span>C</span>
+                        <span>M</span>
+                    </div>
+                    <div class="space-x-2">
+                        <span>2</span>
+                        <span>0</span>
+                        <span>2</span>
+                        <span>3</span>
+                    </div>
+                </div>
+            </aside>
+        </ng-container>
     `,
     styles: [
         `
@@ -95,7 +118,7 @@ import { HomeConstantsService } from 'src/app/shared/constants/home-constants.se
                 @apply h-full flex;
 
                 .nav-button {
-                    @apply flex items-center gap-4 text-gray-400 tracking-wider font-medium;
+                    @apply h-[3rem] flex items-center gap-4 text-gray-400 tracking-wider font-medium;
 
                     &.active {
                         background-color: var(--primary-color);
@@ -128,23 +151,23 @@ import { HomeConstantsService } from 'src/app/shared/constants/home-constants.se
         `,
     ],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: [HomeConstantsService],
+    providers: [HomeConstantsService, AccountConstantsService],
 })
 export class HomeComponent {
+    public user$: Observable<IUserState> = new Observable<IUserState>();
     public items: MenuItem[] = [];
     public isPostsActive: boolean = true;
     public isProfileActive: boolean = false;
 
     constructor(
+        public accountConstants: AccountConstantsService,
         public homeConstants: HomeConstantsService,
         private _store: Store
     ) {
         this._store.select(selectAccessToken).subscribe((accessToken) => {
             console.log(accessToken);
         });
-        this._store.select(selectUser).subscribe((user) => {
-            console.log(user);
-        });
+        this.user$ = this._store.select(selectUser);
         this.items = [
             {
                 label: 'Update Profile',
@@ -157,7 +180,11 @@ export class HomeComponent {
                 command: () => {},
             },
             { separator: true },
-            { label: 'Logout', icon: 'pi pi-sign-out', routerLink: ['/setup'] },
+            {
+                label: 'Logout',
+                icon: 'pi pi-sign-out',
+                routerLink: [accountConstants.loginEndpoint],
+            },
         ];
     }
 
