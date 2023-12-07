3a7ef2be1fc3502037cc9bd44ab51ef5513691f1
Zehrei Verj <moraleszehreiverj8@gmail.com>
2023-12-07 14:31:37 +0800

    delete user profile response

--- PostIt.Backend/PostIt.Contracts/src/PostIt.Contracts/Users/Responses/UserProfileResponse.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections.Generic;
-using PostIt.Contracts.Posts.Responses;
-
-namespace PostIt.Contracts.Users.Responses;
-
-public sealed record UserProfileResponse(UserResponse User, IEnumerable<PostResponse> Posts);
